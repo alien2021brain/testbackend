dec5820707b1ad16d66c801708831bb10427610a
Sachin_Pandey <sachinpandey262523@gmail.com>
2022-09-27 14:12:46 +0530

    add

--- controller/payment-controller.js
+++ controller/payment-controller.js
@@ -57,7 +57,7 @@ export const paymentResponse = (request, response) => {
 
                     post_res.on('end', function () {
                         let result = JSON.parse(res)
-                        response.redirect(`http://localhost:3000/`)
+                        response.redirect(`https://exquisite-strudel-179464.netlify.app/`)
                     });
                 });
                 post_req.write(post_data);
